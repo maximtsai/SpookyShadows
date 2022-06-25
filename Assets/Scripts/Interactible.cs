261765e0a16319369244cb0a088770e02d123b17
Maxim <maximtsai@yahoo.com>
2022-06-25 12:29:44 -0700

    new scene

--- Assets/Scripts/Interactible.cs
+++ Assets/Scripts/Interactible.cs
@@ -5,14 +5,32 @@ using UnityEngine;
 public class Interactible : MonoBehaviour
 {
     // Start is called before the first frame update
+    //Collider m_ObjectCollider;
+    public GameObject letter;
+    private bool isHovered = false;
     void Start()
     {
-        
+        //m_ObjectCollider = GetComponent<Collider>();
+    }
+
+    void OnTriggerEnter(Collider other) {
+        letter.SetActive(true);
+        isHovered = true;
+        Debug.Log("Heya");
+    }
+    void OnTriggerExit(Collider other) {
+        // Destroy everything that leaves the trigger
+        letter.SetActive(false);
+        isHovered = false;
+        Debug.Log("Outta");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown("e") && isHovered)
+        {
+            Destroy(gameObject);
+        }
     }
 }
