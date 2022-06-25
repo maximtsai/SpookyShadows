27ac7ddb09fd7941318dac1f4f5018826b0a46d6
Maxim <maximtsai@yahoo.com>
2022-06-25 14:03:55 -0700

    Adding interative portrait pieces script

--- Assets/Scripts/PieceOneScript.cs
+++ Assets/Scripts/PieceOneScript.cs
@@ -2,27 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Interactible : MonoBehaviour
+public class PieceOneScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    //Collider m_ObjectCollider;
-    public GameObject letter;
+    private bool isHovered = false;
     void Start()
     {
 
     }
 
     void OnTriggerEnter(Collider other) {
-        letter.SetActive(true);
+        isHovered = true;
     }
     void OnTriggerExit(Collider other) {
         // Destroy everything that leaves the trigger
-        letter.SetActive(false);
+        isHovered = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown("e") && isHovered)
+        {
+            ScriptFunc();
+            Destroy(gameObject);
+        }
+    }
+
+    void ScriptFunc() {
 
     }
 }
